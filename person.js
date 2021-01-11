4a119b909b2515ba3cacb9ffccfdcd6dc6ab0358
Coder44595 <62020342+Coder44595@users.noreply.github.com>
2021-01-11 16:11:28 +0000

    Add files via upload

---
+++ person.js
@@ -0,0 +1,16 @@
+const charles = {
+   firstName: 'charles',
+   lastName: 'windsor',
+};
+
+const diana = {
+    firstName: 'Diana',
+    lastName: 'Spencer',
+};
+
+const William = {
+     firstName: 'william',
+     lastName: 'windsor',
+     parents: [charles, diana] 
+};
+module.exports = {charles, diana, William}
